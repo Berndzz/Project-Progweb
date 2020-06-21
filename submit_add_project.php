f32d4d1de42185414b3759958de600ab0854660f
Yohanesrinto <63203011+yohanesrinto@users.noreply.github.com>
2020-06-21 23:13:13 +0700

    masih gagal upload image ke database

--- submit_add_project.php
+++ submit_add_project.php
@@ -32,24 +32,38 @@ if($_POST){
         $validate = false;
     }
 
-    //Pengecekan Upload Image
-    if($image != "jpeg" || $image != "png"){
-        $msg .="Format Image harus PNG/JPEG/JPG. <br \>";
-        $validate = false;
-    }
-
     if($validate){
         $sql = "INSERT INTO gallery_project (title,sub_title) VALUES ('".$title."', '".$sub_title."')";
-        if(mysqli_query($con, $sql)){
-            if(move_uploaded_file($tmp_file, $path)){
+        if(mysqli_query($con, $sql)){  
+        //Pengecekan Upload Image
+        if(strtolower(pathinfo($image, PATHINFO_EXTENSION)) == "image/jpeg" || strtolower(pathinfo($image, PATHINFO_EXTENSION)) == "image/png"){ // Cek apakah tipe file yang diupload adalah JPG / JPEG / PNG
+            // Jika tipe file yang diupload JPG / JPEG / PNG, lakukan :
+              // Proses upload
+              if(move_uploaded_file($tmp_file, $path)){ // Cek apakah gambar berhasil diupload atau tidak
+                // Jika gambar berhasil diupload, Lakukan :  
+                // Proses simpan ke Database
                 $sql = "INSERT INTO gallery_project(image) VALUES('".$image."')";
-                mysqli_query($con, $sql);
-                header("location:form_project.php?msg=1");
-            }else{
-                $msg .= "Maaf, Image gagal untuk diupload. <br \>";
+                if(mysqli_query($con, $sql)){ // Cek jika proses simpan ke database sukses atau tidak
+                  // Jika Sukses, Lakukan :
+                  header("location:form_project.php?msg=1"); // Redirectke halaman index.php
+                }else{
+                  // Jika Gagal, Lakukan :
+                  header("location:form_project.php?msg=0");
+                  $msg .="Maaf, Terjadi kesalahan saat mencoba untuk menyimpan data ke database. <br \>";
+                  $validate = false;
+                }
+              }else{
+                // Jika gambar gagal diupload, Lakukan :
                 header("location:form_project.php?msg=0");
-            }
-            
+                $msg .="Image gagal untuk diupload. <br \>";
+                $validate = false;
+              }
+        }else{
+            // Jika tipe file yang diupload bukan JPG / JPEG / PNG, lakukan :
+            header("location:form_project.php?msg=0");
+            $msg .="Tipe image yang diupload harus berformat JPG / JPEG / PNG. <br \>";
+            $validate = false;
+        }  
         }else{
             header("location:form_project.php?msg=0");
         }
